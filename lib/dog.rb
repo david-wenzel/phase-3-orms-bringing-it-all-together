1cfe3fe652628988f9fad4b2f3dc7e01256f3b5d
David Wenzel <dwenzel95@gmail.com>
2022-12-19 19:38:45 -0600

    commit tree 24ffee9d9a72df3b0a88b25f4a91c06e561486ef

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,44 @@
 class Dog
 
+attr_accessor :name, :breed, :id
+
+def initialize(id: nil, name:, breed:)
+    @id = id
+    @name = name
+    @breed = breed
+end
+
+def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS dogs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        breed TEXT
+      )
+    SQL
+
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    DB[:conn].execute("DROP TABLE IF EXISTS dogs")
+  end
+
+  def save
+    sql = <<-SQL
+      INSERT INTO dogs (name, breed)
+      VALUES (?, ?)
+    SQL
+
+    # insert the song
+    DB[:conn].execute(sql, self.name, self.breed)
+
+    # get the song ID from the database and save it to the Ruby instance
+    self.id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+
+    # return the Ruby instance
+    self
+    end
+
+
 end
